b5b6bf3d629df105a2b7360cbba7a757936222fc
Craig <indigomx9@gmail.com>
2022-10-15 13:01:02 -0400

    Created the pages: Home, Contact, Pricing, and Training components.

---
+++ src/pages/Pricing.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+export const Pricing = (): JSX.Element => {
+    return (
+        <React.Fragment>
+            <h1>Pricing</h1>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit mollitia voluptatum architecto quidem excepturi quo eos id sed facilis aperiam officia eius, impedit nostrum magni autem est? Mollitia, autem quam.</p>
+        </React.Fragment>
+    );
+};
+
+
+
